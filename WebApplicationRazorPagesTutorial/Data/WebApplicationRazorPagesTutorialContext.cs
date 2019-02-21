6de21ee2a8e77aa962620b15e53d38de65f75279
John Green <johngreen8828@yahoo.com>
2019-02-21 10:37:12 -0600

    Added Movie Class, Added Scaffolding for CRUD, Added Database Migration in PM and was registered in the dependency injection container

---
+++ WebApplicationRazorPagesTutorial/Data/WebApplicationRazorPagesTutorialContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplicationRazorPagesTutorial.Models
+{
+    public class WebApplicationRazorPagesTutorialContext : DbContext
+    {
+        public WebApplicationRazorPagesTutorialContext (DbContextOptions<WebApplicationRazorPagesTutorialContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplicationRazorPagesTutorial.Models.Movie> Movie { get; set; }
+    }
+}
