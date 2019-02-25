d2b2441c8ba645d72406b69ccae9bcb097e38c32
John Green <johngreen8828@yahoo.com>
2019-02-25 08:54:42 -0600

    Added DataAnnotations for Display ReleaseDate to Release Date and Column format for Price to 18, 2

--- WebApplicationRazorPagesTutorial/Models/Movie.cs
+++ WebApplicationRazorPagesTutorial/Models/Movie.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
-
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApplicationRazorPagesTutorial.Models
 {
@@ -12,9 +12,12 @@ namespace WebApplicationRazorPagesTutorial.Models
         public int ID { get; set; }
         public string Title { get; set; }
 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
