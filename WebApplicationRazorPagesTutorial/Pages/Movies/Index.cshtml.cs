e753d345831c11374afcf9b5451707b16bb60f63
John Green <johngreen8828@yahoo.com>
2019-02-25 14:37:27 -0600

    Refactored SearchString in @Page directive for Movie/Index.chtml.  Replaced it with search capability in the page for title and genre and filtering

--- WebApplicationRazorPagesTutorial/Pages/Movies/Index.cshtml.cs
+++ WebApplicationRazorPagesTutorial/Pages/Movies/Index.cshtml.cs
@@ -29,11 +29,22 @@ namespace WebApplicationRazorPagesTutorial.Pages.Movies
 
         public async Task OnGetAsync()
         {
-            var movies = from m in _context.Movie select m;
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
+            var movies = from m in _context.Movie
+                         select m;
+
             if (!string.IsNullOrEmpty(SearchString))
             {
                 movies = movies.Where(s => s.Title.Contains(SearchString));
             }
+            if (!string.IsNullOrEmpty(MovieGenre))
+            {
+                movies = movies.Where(x => x.Genre == MovieGenre);
+            }
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
             Movie = await movies.ToListAsync();
         }
     }
